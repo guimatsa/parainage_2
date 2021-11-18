50608522d88b78afc84cd50a214f1dddce40247c
Dllite <dilanzambou2@gmail.com>
2021-11-18 06:07:47 +0100

    ok

---
+++ controller/home.php
@@ -0,0 +1,10 @@
+<?php
+require_once "../includes/db.php";
+
+require_once "../view/head_view.php";
+
+require_once "../view/head_view_index.php";
+
+require_once "../view/header_view.php";
+
+require_once "../_fonction/fontion.php";
