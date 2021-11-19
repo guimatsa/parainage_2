ce712ab48ba614aff90ef827d43de620819f4866
Dllite <dilanzambou2@gmail.com>
2021-11-19 21:13:13 +0100

    Update login.php

--- controller/login.php
+++ controller/login.php
@@ -37,8 +37,6 @@
             $ligne2=$query_log2->fetch_assoc();
             $dpassword=$ligne['password'];
             $dpassword2=$ligne2['password'];
-            
-            
 
             if(password_verify($password, $dpassword)){
                 $nom=$ligne['nom'];
@@ -60,3 +58,24 @@
     require_once "../view/login_view.php";
 
     ?>
+
+
+
+<!-- Footer -->
+<body class="d-flex flex-column">
+  <div id="page-content">
+    <div class="container text-center">
+      <div class="row justify-content-center">
+        <div class="col-md-7">
+          <h1 class="fw-light mt-4 text-white">Sticky Footer using Flexbox</h1>
+          <p class="lead text-white-50">Use just two Bootstrap utility classes and three custom CSS rules and you will have a flexbox enabled sticky footer for your website!</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <footer id="sticky-footer" class="flex-shrink-0 py-4 bg-dark text-white-50">
+    <div class="container text-center">
+      <small>Copyright &copy; Your Website</small>
+    </div>
+  </footer>
+</body>
