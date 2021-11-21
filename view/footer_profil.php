f6f8b0064425a19c2af05d90abe80a70ade84c02
Dllite <dilanzambou2@gmail.com>
2021-11-21 08:14:53 +0100

    OK

---
+++ view/footer_profil.php
@@ -0,0 +1,20 @@
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+    <div class="copyright">
+      &copy; Copyright <strong><span>LITE CORP</span></strong>. Tous drois réservés.
+    </div>
+    <div class="credits">
+      Designed by <a href="https://bootstrapmade.com/">JULIO GUIMATSA</a>
+    </div>
+  </footer><!-- End Footer -->
+
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main-profil.js"></script>
+
+</body>
+
+</html>
